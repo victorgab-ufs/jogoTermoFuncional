5236010fc0b9f15f1751de9688bd7c8d1961724a
Leonardodcomp <leonardo.souzasilva@dcomp.ufs.br>
2025-08-17 11:24:18 -0300

    Divisao do projeto

---
+++ script.js
@@ -0,0 +1,22 @@
+// Parte de Renato (Sempre deixe 10 linhas de distancias para a parte do colega seguinte)
+
+
+
+
+
+
+
+
+
+// Parte de Leonardo (Sempre deixe 10 linhas de distancias para a parte do colega seguinte)
+
+
+
+
+
+
+
+
+
+
+// Parte de Victor.
