b6b9561ea4b61f64d4edfc4d133c814d4e5bf51c
Leonardodcomp <leonardo.souzasilva@dcomp.ufs.br>
2025-08-17 11:54:06 -0300

    return funcao funcional

--- script.js
+++ script.js
@@ -14,6 +14,37 @@
 
 
 
+ // Com o acumulador e o reduce que sao a "alma" do jogo programado, faremos o return para fechar a função.
+
+  return tentativa.split("").map((letra, i) => {
+
+      //De forma simples, se for isso, retorne isso.
+        //Esse if abaixo representará o melhor dos casos, letra certa na posição certa
+    if (resultadoParcial[i]) return resultadoParcial[i];
+
+
+    //Se a letra existir, mas estiver na posição errada, precisamos mostrar isso.
+    
+    const indexPresente = letrasRestantes.indexOf(letra);
+
+     //Sabemos pelas listas de exercício: lista 5, questão 4 que o index de um elemento inexistente é -1, vamos usar isso.
+    if (indexPresente !== -1) {
+      //Aqui a logica usada é a seguinte, a letra existe, o jogador acertou, ta na posição errada, mas acertou, então precisamos retirar ela da lista letrasRestantes
+      letrasRestantes[indexPresente] = null;
+      return { letra, status: "present" };
+    }
+    // Se nada deu certo é porque a letra nem existe na palavra, então
+    return { letra, status: "absent" };
+  });
+
+  //Chegamos ao fim da função verificarJogada 
+
+
+
+
+
+
+
 
 
 
